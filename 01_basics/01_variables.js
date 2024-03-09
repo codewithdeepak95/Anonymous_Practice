7d1ba1edc90eb88195d508cff02d06624a42f36f
codewithdeepak95 <158283315+codewithdeepak95@users.noreply.github.com>
2024-03-09 13:52:59 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const userId = 345435
+let userEmail = "lovejangid@gmail.com"
+var userPass = "12356"
+userCity = "Jaipur"
+let userState;
+
+userEmail = "wwww.codpro.com"
+userPass = "232332"
+userCity = "Udaipur"
+// userId = 3 not allowed
+
+/* 
+Prefer not to use var keyword
+bec of issue in block and functional scope
+*/
+
+console.table([userId, userEmail, userPass, userCity, userState])
